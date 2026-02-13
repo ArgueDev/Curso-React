a471be73b6166bfa93e66a2457713e30fb9fa461
ArgueDev <christianarguelloo97@gmail.com>
2026-02-12 23:04:50 -0500

    Nuevo Proyecto iniciado

---
+++ clima-react-typescript/src/components/Form/Form.tsx
@@ -0,0 +1,77 @@
+import { 
+    useState, 
+    type ChangeEvent, 
+    type SubmitEvent 
+} from "react";
+
+import { countries } from "../../data/countries";
+import styles from './Form.module.css';
+import type { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
+
+type FormProps = {
+    fetchWeather: () => void
+}
+
+export default function Form({fetchWeather} : FormProps) {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    });
+
+    const [alert, setAlert] = useState('');
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (Object.values(search).includes('')) {
+            setAlert('Todos los campos son obligatorios');
+            return;
+        }
+
+        fetchWeather()
+    }
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit}>
+            {alert && <Alert>{alert}</Alert>}
+            <div className={styles.field}>
+                <label htmlFor="city">Ciudad:</label>
+                <input
+                    id='city'
+                    type="text"
+                    name="city"
+                    placeholder="Ciudad"
+                    value={search.city}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className={styles.field}>
+                <label htmlFor="country">País:</label>
+                <select
+                    id='country'
+                    name="country"
+                    value={search.country}
+                    onChange={handleChange}
+                >
+                    <option value="">-- Seleccione un país --</option>
+                    {countries.map(country => (
+                        <option
+                            key={country.code}
+                            value={country.code}
+                        >{country.name}</option>
+                    ))}
+                </select>
+            </div>
+            <input className={styles.submit} type="submit" value='Consultar Clima' />
+        </form>
+    )
+}

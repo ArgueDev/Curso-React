10557108adf715d238dc13248139d40635ab68cb
ArgueDev <christianarguelloo97@gmail.com>
2026-02-14 22:33:57 -0500

    Instalacion de zod para el schema

--- clima-react-typescript/src/components/Form/Form.tsx
+++ clima-react-typescript/src/components/Form/Form.tsx
@@ -10,7 +10,7 @@ import type { SearchType } from "../../types";
 import Alert from "../Alert/Alert";
 
 type FormProps = {
-    fetchWeather: () => void
+    fetchWeather: (search: SearchType) => Promise<void>
 }
 
 export default function Form({fetchWeather} : FormProps) {
@@ -37,7 +37,7 @@ export default function Form({fetchWeather} : FormProps) {
             return;
         }
 
-        fetchWeather()
+        fetchWeather(search)
     }
 
     return (
--- clima-react-typescript/src/hooks/useWeather.ts
+++ clima-react-typescript/src/hooks/useWeather.ts
@@ -1,7 +1,43 @@
+import axios from 'axios';
+import { z } from 'zod';
+
+import type { SearchType } from '../types';
+
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number()
+    })
+});
+
+type Weather = z.infer<typeof Weather>
+
 export default function useWeather() {
     
-    const fetchWeather = () => {
-        console.log('Consultando...');
+    const fetchWeather = async (search: SearchType) => {
+
+        const appid = import.meta.env.VITE_API_KEY
+
+        try {
+            const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appid}`
+            const {data} = await axios(geoURL);
+
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`
+            const {data: weatherResult} = await axios(weatherUrl);
+            const result = Weather.safeParse(weatherResult);
+            
+            if (result.success) {
+                console.log(result.data.name);
+            }
+
+        } catch (error) {
+            console.error(error)
+        }
     }
     
     return {

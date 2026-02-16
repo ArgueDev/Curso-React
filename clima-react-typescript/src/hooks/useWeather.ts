cf6bf9d6e0a4b598eda3e77d102fad198be90d00
ArgueDev <christianarguelloo97@gmail.com>
2026-02-15 20:27:16 -0500

    Proyecto finalizado

--- clima-react-typescript/src/hooks/useWeather.ts
+++ clima-react-typescript/src/hooks/useWeather.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { z } from 'zod';
 
 import type { SearchType } from '../types';
+import { useMemo, useState } from 'react';
 
 const Weather = z.object({
     name: z.string(),
@@ -12,35 +13,65 @@ const Weather = z.object({
     })
 });
 
-type Weather = z.infer<typeof Weather>
+const initialState = {
+    name: '',
+    main: {
+        temp: 0,
+        temp_max: 0,
+        temp_min: 0
+    }
+}
+
+export type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
-    
+
+    const [weather, setWeather] = useState<Weather>(initialState);
+    const [loading, setLoading] = useState(false);
+    const [notFound, setNotFound] = useState(false);
+
     const fetchWeather = async (search: SearchType) => {
 
         const appid = import.meta.env.VITE_API_KEY
 
+        setLoading(true);
+        setWeather(initialState);
+
         try {
-            const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appid}`
-            const {data} = await axios(geoURL);
+            const geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appid}`
+            const { data } = await axios(geoURL);
+
+            // Comprobamos si existe
+            if (!data[0]) {
+                setNotFound(true);
+                return;
+            }
 
             const lat = data[0].lat;
             const lon = data[0].lon;
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`
-            const {data: weatherResult} = await axios(weatherUrl);
+            const { data: weatherResult } = await axios(weatherUrl);
             const result = Weather.safeParse(weatherResult);
-            
+
             if (result.success) {
-                console.log(result.data.name);
+                setWeather(result.data);
             }
 
         } catch (error) {
             console.error(error)
+        } finally {
+            setLoading(false);
         }
     }
-    
+
+    const hasWeatherData = useMemo(() => weather.name, [weather])
+
     return {
-        fetchWeather
+        weather,
+        loading,
+        notFound,
+        fetchWeather,
+        hasWeatherData
     }
 }

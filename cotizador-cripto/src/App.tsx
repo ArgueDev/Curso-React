67b1205e600a90be597644df18060c2050140d6d
ArgueDev <christianarguelloo97@gmail.com>
2026-04-05 21:11:12 -0500

    Inicio de proyecto de Cotizador de Cryptomonedas

---
+++ cotizador-cripto/src/App.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import CriptoSearchForm from "./components/CriptoSearchForm";
+import { useCryptoStore } from "./store";
+
+export default function App() {
+
+  const fetchCrypt = useCryptoStore((state) => state.fetchCrypto);
+
+  useEffect(() => {
+    fetchCrypt();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
+        <div className="content">
+          <CriptoSearchForm />
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ cotizador-cripto/src/components/CriptoSearchForm.tsx
@@ -0,0 +1,31 @@
+import { currencies } from "../data";
+
+export default function CriptoSearchForm() {
+    return (
+        <form action="" className="form">
+            <div className="field">
+                <label htmlFor="currency">Moneda:</label>
+                <select name="currency" id="currency">
+                    <option value="">-- Seleccione --</option>
+                    {currencies.map(currency => (
+                        <option
+                            value={currency.code}
+                            key={currency.code}
+                        >
+                            {currency.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="field">
+                <label htmlFor="cryptocurrency">Criptomoneda:</label>
+                <select name="cryptocurrency" id="cryptocurrency">
+                    <option value="">-- Seleccione --</option>
+                </select>
+            </div>
+
+            <input type="submit" value='Cotizar' />
+        </form>
+    );
+}
